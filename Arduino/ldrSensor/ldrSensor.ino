f04c1325e5d7720b446fa8657b41a9d3f3122d3e
Prerna Sharma <34606575+preru98@users.noreply.github.com>
2018-08-30 00:22:22 +0530

    Second commit

--- Arduino/ldrSensor/ldrSensor.ino
+++ Arduino/ldrSensor/ldrSensor.ino
@@ -1,28 +1,34 @@
 #define SENSOR A0
-#define LED 12
-#define LED 8
+#define LED1 12
+#define LED2 8
+
 void setup()
 {
   pinMode(SENSOR,INPUT);
-  pinMode(LED ,OUTPUT);
+  pinMode(LED1 ,OUTPUT);
+  pinMode(LED2 ,OUTPUT);
   Serial.begin(9600);
 } 
  
 
 void loop()
-{ int AnalogValue;
-AnalogValue = analogRead(A0);
-Serial.println(AnalogValue);
+{ 
+  int AnalogValue;
+  AnalogValue = analogRead(SENSOR);
+  Serial.println(AnalogValue);
 
-if(AnalogValue==0)
- {digitalWrite(12,LOW);
- digitalWrite(8,LOW);
- delay(100);
+  if(AnalogValue==0)
+ {
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  delay(100);
  }
+ 
  else
- {digitalWrite(12,HIGH);
- digitalWrite(8,HIGH);
- delay(100);
+ {
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,HIGH);
+  delay(100);
  }
 }
 
